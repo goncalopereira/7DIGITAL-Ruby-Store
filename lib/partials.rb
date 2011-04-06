7b6683fea03e990261c1ba5573c3c951165f7a98
Goncalo Pereira <goncalo.pereira@gmail.com>
2011-04-06 15:16:20 +0100

    move player and release info to partials

---
+++ lib/partials.rb
@@ -0,0 +1,20 @@
+#modified from:
+#http://stackoverflow.com/questions/3974878/rendering-a-partial-from-a-haml-file
+module Haml
+  module Helpers
+    def partial(template, *args)
+      template_array = template.to_s.split('/')
+      template = template_array[0..-2].join('/') + "/#{template_array[-1]}"
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      options.merge!(:layout => false)
+      if collection = options.delete(:collection) then
+        collection.inject([]) do |buffer, member|
+          buffer << haml(:"#{template}", options.merge(:layout =>
+          false, :locals => {template_array[-1].to_sym => member}))
+        end.join("\n")
+      else
+        haml(:"#{template}", options)
+      end
+    end
+  end
+end
