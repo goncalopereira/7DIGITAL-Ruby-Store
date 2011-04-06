ffa72bb9f5cfd0e87bd573356157fd49f442b066
Goncalo Pereira <goncalo.pereira@gmail.com>
2011-04-06 13:56:28 +0100

    add artist and track results, artist page still not implemented

--- routes.rb
+++ routes.rb
@@ -29,18 +29,28 @@ end
 post '/search/:country' do |country|
 
 	search_value = params[:search_value]
-	
+	page_size = params[:number_results]
+
 	credentials = Credentials.new
 	@api_client = get_api_client credentials, country
 	
-	releases = @api_client.release.search(search_value, :page_size=>20)
-	
+	releases = @api_client.release.search(search_value, :page_size=>page_size)
+  artists = @api_client.artist.search(search_value, :page_size=>page_size)
+  tracks = @api_client.track.search(search_value, :page_size=>page_size)
+
 	@model = SearchModel.new
 	@model.country = country
 	@model.search_value = search_value
 	@model.releases = releases
+  @model.artists = artists
+  @model.tracks = tracks
+  @model.page_size = page_size
 
 	haml :search_results
+end
+
+get '/:country/artist/:id' do |country,artist_id|
+  "#{country}, #{artist_id}, artist page not implemented yet..."
 end
 
 get '/:country/:id'  do |country,release_id|
