26061e071e0fb5b8a2c81510b50441d08d49e8b2
Goncalo Pereira <goncalo.pereira@gmail.com>
2011-04-06 12:59:58 +0100

    clean up formatting into model deleting unused local domain track

--- models/release_model.rb
+++ models/release_model.rb
@@ -2,15 +2,15 @@ load 'lib/js_strings.rb'
 
 class ReleaseModel	
 
-	attr_accessor :image_url,:release_name,:js_array_tracks,:artist_name,:url, :release_date, :label
+	attr_accessor :image_url,:release_name,:tracks,:artist_name,:url, :release_date, :label
 
-	def initialize release_image,artist_name,release_title,release_url,tracks
-		@image_url = release_image
-		@release_name = release_title
-		js_string_helper = JSStrings.new
-		@js_array_tracks = js_string_helper.track_list_string(tracks)
-		@artist_name = artist_name
-		@url = release_url
-	end
+  def formatted_date
+    @release_date.strftime("%d/%m/%Y")
+  end
+
+  def js_array_tracks
+    js_string_helper = JSStrings.new
+    js_string_helper.track_list_string(tracks)
+  end
 	
 end
--- routes.rb
+++ routes.rb
@@ -5,7 +5,6 @@ require 'haml'
 load 'lib/very_simple_cache.rb'
 load 'lib/js_strings.rb'
 load 'lib/credentials.rb'
-load 'domain/track.rb'
 load 'models/release_model.rb'
 load 'models/search_model.rb'
 
@@ -53,9 +52,14 @@ get '/:country/:id'  do |country,release_id|
 	release = @api_client.release.get_details(release_id,options)
 	release_tracks = @api_client.release.get_tracks(release_id)
 
-	@model = ReleaseModel.new(release.image,release.artist.name,release.title,release.url,release_tracks)
+	@model = ReleaseModel.new
   @model.label = release.label.name
-  @model.release_date = release.release_date.strftime("%d/%m/%Y")
+  @model.release_date = release.release_date
+  @model.image_url = release.image
+	@model.release_name = release.title
+	@model.tracks = release_tracks
+	@model.artist_name = release.artist.name
+	@model.url = release.url
 
 	haml :release
 end
