530df572359188cd17dca31bf2fc77d0794e0732
Goncalo Pereira <goncalo.pereira@gmail.com>
2011-04-07 18:03:09 +0100

    basket add and remove to get

--- routes.rb
+++ routes.rb
@@ -66,22 +66,32 @@ post '/search' do
 	haml :search_results
 end
 
-post '/basket/add' do
+get '/basket/add/:release_id/:track_id' do |release_id,track_id|
   country = get_country
   credentials = Credentials.new
   api_client = get_api_client credentials, country
 
-  session[:basket] = api_client.basket.add_item(session[:basket].id,params[:release_id], params[:track_id])
+  session[:basket] = api_client.basket.add_item(session[:basket].id,release_id, track_id)
 
   redirect "/#{country}"
 end
 
-post '/basket/remove' do
+get '/basket/add/:release_id' do |release_id|
   country = get_country
   credentials = Credentials.new
   api_client = get_api_client credentials, country
 
-  session[:basket] = api_client.basket.remove_item(session[:basket].id,params[:item_id])
+  session[:basket] = api_client.basket.add_item(session[:basket].id,release_id)
+
+  redirect "/#{country}"
+end
+
+get '/basket/remove/:item_id' do  |item_id|
+  country = get_country
+  credentials = Credentials.new
+  api_client = get_api_client credentials, country
+
+  session[:basket] = api_client.basket.remove_item(session[:basket].id,item_id)
 
   redirect "/#{country}"
 end
