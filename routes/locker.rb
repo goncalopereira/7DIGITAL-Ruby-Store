5e4127746ab026c9a9bdb48a1edb9fe7e514058f
goncalo <goncalo@goncalo-VirtualBox.(none)>
2011-04-08 20:06:11 +0100

    add stream track for locker... not working with html5 audio tag in FF... url works in browser

--- routes/locker.rb
+++ routes/locker.rb
@@ -1,4 +1,8 @@
 get '/locker' do
+	redirect '/locker/0/0'
+end
+
+get '/locker/:release_id/:track_id' do |release_id,track_id|
   country = get_country
   credentials = Credentials.new
   api_client = get_api_client credentials, country
@@ -6,10 +10,15 @@ get '/locker' do
   @user = session[:user]
   @basket = get_basket api_client
 
-  if @user != nil
-    puts @user.oauth_access_token
-    @locker = api_client.user.get_locker(@user.oauth_access_token)
-    haml :locker
+	if @user != nil
+   	
+		@locker = api_client.user.get_locker(@user.oauth_access_token)
+    
+		if release_id != 0
+			@player_track_url = api_client.user.get_stream_track_url(release_id, track_id, @user.oauth_access_token)
+		end
+		
+	haml :locker
   else
     redirect '/'
   end
