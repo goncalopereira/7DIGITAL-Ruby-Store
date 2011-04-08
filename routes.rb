be6ac1d51507adf8ce6cb3e88a4d9ec1e8083c39
Goncalo Pereira <goncalo.pereira@gmail.com>
2011-04-08 17:40:16 +0100

    added example for locker first page

--- routes.rb
+++ routes.rb
@@ -115,6 +115,23 @@ get '/logout' do
   redirect "/"
 end
 
+get '/locker' do
+  country = get_country
+  credentials = Credentials.new
+  api_client = get_api_client credentials, country
+
+  @user = session[:user]
+  @basket = get_basket api_client
+
+  if @user != nil
+    puts @user.oauth_access_token
+    @locker = api_client.user.get_locker(@user.oauth_access_token)
+    haml :locker
+  else
+    redirect '/'
+  end
+end
+
 get '/artist/:id' do |artist_id|
   country = get_country
   credentials = Credentials.new
