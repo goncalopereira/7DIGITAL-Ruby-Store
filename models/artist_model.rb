3c61e1588f5b50c6a57759d59756fb4ee32e8547
goncalo pereira <goncalo.pereira@gmail.com>
2011-04-10 15:47:12 +0100

    similar artists, added top tracks call to route

--- models/artist_model.rb
+++ models/artist_model.rb
@@ -1,4 +1,4 @@
 class ArtistModel
 
-  attr_accessor :image_url,:artist_name, :url, :releases
+  attr_accessor :image_url,:artist_name, :url, :releases, :similar_artists, :top_tracks
 end
--- routes/artist.rb
+++ routes/artist.rb
@@ -1,15 +1,23 @@
 get %r{/artist/([0-9]+)} do |artist_id|
-  options = {}
-  options[:imageSize] = @main_image_size
+  	options = {}
+  	options[:imageSize] = @main_image_size
 
-  artist = @api_client.artist.get_details(artist_id,options)
-  artist_releases = @api_client.artist.get_releases(artist_id, options={})
+	artist = @api_client.artist.get_details(artist_id,options)
+	
+	pass unless !artist.nil? 
 
-  model = ArtistModel.new
-  model.image_url = artist.image
-  model.url = artist.url
-  model.artist_name = artist.name
-  model.releases = artist_releases
+  	artist_releases = @api_client.artist.get_releases(artist_id, options={})
 
-  haml :artist, :locals => {:model => model}
+	similar_artists = @api_client.artist.get_similar(artist_id)
+	top_tracks = @api_client.artist.get_top_tracks(artist_id)
+
+  	model = ArtistModel.new
+  	model.image_url = artist.image
+  	model.url = artist.url
+  	model.artist_name = artist.name
+  	model.releases = artist_releases
+	model.similar_artists = similar_artists
+	model.top_tracks = top_tracks
+
+	haml :artist, :locals => {:model => model}
 end
