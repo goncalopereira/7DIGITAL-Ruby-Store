92792b0142ead248ad99eb3ea32d78adaeaf697e
goncalo pereira <goncalo.pereira@gmail.com>
2011-04-10 17:03:13 +0100

    tiny clean on credentials lib

--- lib/credentials.rb
+++ lib/credentials.rb
@@ -1,22 +1,16 @@
 class Credentials
-	def key
-		@key
-	end
-	
-	def secret
-		@secret
-	end
+	attr_accessor :key, :secret
 	
 	def initialize
-		credentials_file_name = "credentials"
 		
+		credentials_file_name = 'credentials'
+
 		if File.exists?(credentials_file_name)				
 			file = File.new(credentials_file_name,"r")	
 			@key = file.gets.split.join("\n")
 			@secret = file.gets.split.join("\n")
 		else
 			throw  'no credentials file'
-			
 		end
 	end
 		
