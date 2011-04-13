9f40de09a9782bc82f85bc414a786b07448a1819
goncalo pereira <goncalo.pereira@gmail.com>
2011-04-13 19:11:45 +0100

    download url is only generated when requested, prevents timeouts and extra signing load vs getting searching locker

--- routes/locker.rb
+++ routes/locker.rb
@@ -1,12 +1,30 @@
-get %r{/locker(\/?)([0-9]*)(\/?)([0-9]*)} do |slash,release_id,slash_2,track_id|
+get '/locker' do
    
 	pass if @user.nil?
 	
 	@locker = @api_client.user.get_locker(@user.oauth_access_token)
-    
-	if release_id != '' and track_id != ''
-		@player_track_url = @api_client.user.get_stream_track_url(release_id, track_id, @user.oauth_access_token)
-	end
 	
 	haml :locker
 end		
+
+get %r{/locker/download/([0-9]+)/([0-9]+)} do |release_id,track_id|
+	pass if @user.nil?
+	
+	@locker = @api_client.user.get_locker(@user.oauth_access_token)
+
+	@locker.locker_releases.each do |locker_release|
+		locker_release.locker_tracks.each do |locker_track|
+			if locker_release.release.id == release_id.to_i and locker_track.track.id == track_id.to_i
+				redirect locker_track.download_urls[0].url
+			end 
+		end
+	end	
+
+	pass
+end
+
+get %r{/locker/stream/([0-9]+)/([0-9]+)} do |release_id,track_id|
+	pass if @user.nil?
+
+	redirect @api_client.user.get_stream_track_url(release_id, track_id, @user.oauth_access_token)
+end
